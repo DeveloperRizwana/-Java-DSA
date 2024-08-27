c58c61a7fb77c5f5b964390aa347f8a7e51451ba
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-27 08:52:05 +0530

    new

---
+++ light.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+
+public class light {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+    
+}
