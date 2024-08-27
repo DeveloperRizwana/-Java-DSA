4940994af9e2c1d243994781907935288d6db132
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-27 07:57:21 +0530

    i am delivering

---
+++ tough.java
@@ -0,0 +1,26 @@
+public class tough {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25; 
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+    
+
