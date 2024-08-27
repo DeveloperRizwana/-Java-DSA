9015d48e11091e7e53a37d4226c58a08b1a4ce9a
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-27 08:39:32 +0530

    new

---
+++ hard.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+
+public class hard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+
+            
+}
