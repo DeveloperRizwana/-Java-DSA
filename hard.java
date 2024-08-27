204350198dc3f42cb6d191a1d11a6f3238d541ca
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-27 08:45:46 +0530

    new

--- hard.java
+++ hard.java
@@ -5,8 +5,10 @@ import java.util.*;
 public class hard {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println(name);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
     }
 
             
