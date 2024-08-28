70aa42306efb40c9c1e4bcf23bd406dafdb387f1
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-28 08:49:13 +0530

    new

--- greeting.java
+++ greeting.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class late {
+public class greeting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int button = sc.nextInt();
