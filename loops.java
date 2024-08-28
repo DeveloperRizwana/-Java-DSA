f717ba6b6594a2c9e9e9a6d78b1b9bd53d52f677
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-28 13:11:37 +0530

    new

--- loops.java
+++ loops.java
@@ -1,7 +1,12 @@
 public class loops {
     public static void main(String args[]) {
-        for(int counter = 0; counter < 21; counter++) {
-            System.out.println(counter);
-        }    
+      //  for(int counter = 0; counter < 21; counter++) {
+      //      System.out.println(counter);
+      //  } 
+      
+      
+      for(int i = 0; i < 21; i++) {
+        System.out.println(i);
+      }  
     }
 }
