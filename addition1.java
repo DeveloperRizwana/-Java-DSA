2e511f22d42c87178e20d2d16522bb8e2dc91996
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-28 08:45:43 +0530

    new

--- addition1.java
+++ addition1.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 
 
-public class hard {
+public class addition1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
