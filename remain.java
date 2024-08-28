7e848d7f0559cc382c5c9f17ecb24f1017762abe
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-28 08:21:04 +0530

    new

---
+++ remain.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+
+public class remain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int remainder = a % b;
+        System.out.println(remainder);
+    }    
+    
+}
