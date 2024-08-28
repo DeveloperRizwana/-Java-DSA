200f42ee7fbdc0e0947c088b017afbb1a9a32ed3
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-28 08:51:15 +0530

    new

--- string.java
+++ string.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 
 
-public class light {
+public class string {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
