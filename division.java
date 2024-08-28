11c8c8629616522fcf40e8f7c5ccfc22b6f4deaa
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-28 08:15:10 +0530

    new

---
+++ division.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+
+public class division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int division = a / b;
+        System.out.println(division);
+    }    
+
+    
+}
