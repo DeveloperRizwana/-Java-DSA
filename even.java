4d2499855269a66fc213057f40d6d2979e4d5451
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-08-28 08:47:44 +0530

    new

--- even.java
+++ even.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class high {
+public class even {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
