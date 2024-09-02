8f4aa6cad9c2502398615f04ce98b59c57dca35f
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-02 20:57:46 +0530

    new

--- condiional/solution.java
+++ condiional/solution.java
@@ -1,18 +1,19 @@
 import java.util.*;
+	 
+
 
 public class solution {
-    public static void main(String[] argd){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int sum = 0;
-        int i = 0;
-            while(i <= n){
-                sum = sum + i;
-                i = i + 2;
-        }
-        System.out.println(sum);
-            
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		int i = 0;
+		    while(i <= n){
+			sum = sum + i;
+			i = i + 2;
 
-    
-}
+		}	
+		System.out.println(sum);
+    }
+}        
+                
