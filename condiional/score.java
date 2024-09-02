354cc1c9a5239089d8827703aa9a58577d05149c
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-02 06:52:55 +0530

    new

---
+++ condiional/score.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class score {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        if(score >= 90) {
+            System.out.println("A");
+        } else if(score >= 80 && score <= 89){
+            System.out.println("B");
+        } else if(score >= 70 && score <= 79){
+            System.out.println("C");
+        } else if(score >= 60 && score >= 69){
+            System.out.println("D");                
+        } else if(score <= 60){
+            System.out.println("F");
+        }
+    }            
+
+    
+}
