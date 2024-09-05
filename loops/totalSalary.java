0bdafd69b83365174948feaa8a9644320f284404
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-05 18:08:02 +0530

    new

---
+++ loops/totalSalary.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class totalSalary {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int basic = sc.nextInt();
+        char grade = sc.next().charAt(0);
+
+        double hra = 0.2 * basic;
+        double da = 0.5 * basic;
+        int allowance;
+        double pf = 0.11;
+
+        if(grade == 'A'){
+            allowance = 1700;
+        }
+        else if(grade == 'B'){
+            allowance = 1500;
+        }
+        else {
+            allowance = 1300;
+        }
+        
+        double totalSalary = basic + hra + da + allowance - pf;
+        int ans =(int)Math.round(totalSalary);
+        System.out.println(ans);
+    }    
+ 
+}
