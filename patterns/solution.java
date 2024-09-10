5996130a283a3e63c51382b41e75c150a3a85f97
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-10 07:35:33 +0530

    solved square pattern

--- patterns/solution.java
+++ patterns/solution.java
@@ -7,7 +7,7 @@ public class solution {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                System.out.print(i);
+                System.out.print(i + " ");
             }
             System.out.println();
         }
