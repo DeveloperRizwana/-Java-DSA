dd7072bb3e8c1b4003074c131e85e04b31b21840
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-11 21:52:27 +0530

    solved half diamond pattern

---
+++ patterns/halfDiamondPattern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class halfDiamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
+// *
+// ***
+// *****
+// *******
+// *********
