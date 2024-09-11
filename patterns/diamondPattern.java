d00496ca3606e2fe357a386c932fed2117980562
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-11 13:04:55 +0530

    solved diamond pattern problem

---
+++ patterns/diamondPattern.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class diamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // LOWER HALF
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
