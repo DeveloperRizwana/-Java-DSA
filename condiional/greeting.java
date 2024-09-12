0d7668fa5716a83ab4411e24dcb3b716c27ffadf
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-12 22:20:16 +0530

    solved a problem

--- condiional/greeting.java
+++ condiional/greeting.java
@@ -1,19 +1,19 @@
-"import java.util.*;
+import java.util.*;
 
 public class greeting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int button = sc.nextInt();
 
-        if(button == 1) {
+        if (button == 1) {
             System.out.println("Hello");
-        } else if(button == 2) {
+        } else if (button == 2) {
             System.out.println("Namaste");
-        } else if(button == 3) {
+        } else if (button == 3) {
             System.out.println("Bonjour");
         } else {
             System.out.println("Invalid Button");
         }
-    }               
-    
+    }
+
 }
