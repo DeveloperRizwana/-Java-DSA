32db9028e988a48cbfb2a84008cbfd32bee5c37b
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-12 11:49:33 +0530

    practiced pattern

--- patterns/abcd.java
+++ patterns/abcd.java
@@ -1,11 +1,10 @@
 import java.util.*;
 
-public class diamondPattern {
+public class abcd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 5;
+        int n = sc.nextInt();
 
-        // upper half
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
@@ -15,7 +14,6 @@ public class diamondPattern {
             }
             System.out.println();
         }
-        // LOWER HALF
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
@@ -24,7 +22,6 @@ public class diamondPattern {
                 System.out.print("*");
             }
             System.out.println();
-
         }
     }
 }
