042439c8082ceec9c46b20214a8b1b7ff35af7ad
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-15 16:39:19 +0530

    one more problem solved

--- patterns/halfDiamond.java
+++ patterns/halfDiamond.java
@@ -1,6 +1,6 @@
 public class halfDiamond {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 4;
 
         System.out.println("*");
         for (int i = 1; i <= n; i++) {
@@ -15,7 +15,7 @@ public class halfDiamond {
             System.out.println("*");
 
         }
-        for (int i = n; i >= 1; i--) {
+        for (int i = n - 1; i >= 1; i--) {
             System.out.print("*");
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
