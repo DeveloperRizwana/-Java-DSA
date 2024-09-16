d35bdcc3d2c2eee94fa1b14d48c2f9fc25ad1d88
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-16 15:48:50 +0530

    find the factorial of 2 numbers

---
+++ Functions/factorial.java
@@ -0,0 +1,23 @@
+package Functions;
+
+import java.util.*;
+
+public class factorial {
+    public static void printFactorial(int n) {
+        int factorial = 1;
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+        return;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printFactorial(n);
+
+    }
+
+}
