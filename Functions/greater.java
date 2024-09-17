ba71af6621d962b6b9820197536b3052b918e8be
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-17 12:20:13 +0530

    greater of 2 numbers

---
+++ Functions/greater.java
@@ -0,0 +1,24 @@
+package Functions;
+
+import java.util.*;
+
+public class greater {
+    public static int printGreaterNumber(int a, int b) {
+        int greater = Math.max(a, b);
+
+        return greater;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result = printGreaterNumber(a, b);
+
+        System.out.println("Thge greater number is : " + result);
+
+    }
+
+}
--- input output/equal.java
+++ input output/equal.java
@@ -6,14 +6,13 @@ public class equal {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        if(a == b) {
+        if (a == b) {
             System.out.println("Equal");
-        } else if(a > b) {
-                System.out.println("a is greater");
+        } else if (a > b) {
+            System.out.println("a is greater");
+        } else {
+            System.out.println("a is lesser");
         }
-        else {
-                 System.out.println("a is lessert");
-        }
-    }                     
-    
+    }
+
 }
