32b5f97eae67ddfb92151c1e1fb645a0b6a1656b
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-19 13:20:57 +0530

    learned to code operators

---
+++ operatores/reversedDigits.java
@@ -0,0 +1,18 @@
+package operatores;
+
+import java.util.*;
+
+public class reversedDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int ans = 0;
+        while (n > 0) {
+            ans = (ans * 10 + n % 10);
+            n /= 10;
+        }
+        System.out.println(ans);
+    }
+
+}
