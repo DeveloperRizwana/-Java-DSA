3d1935f1f8a08c5d78902368143dd7b2afa7ac49
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-19 15:30:35 +0530

    practiced

--- operatores/sumSereis.java
+++ operatores/sumSereis.java
@@ -6,15 +6,18 @@ public class sumSereis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int i = 1;
 
         int ans = 0;
-        for (int i = 1; i <= n; i++) {
+        // for (int i = 1; i <= n; i++) {
+        while (i <= n) {
             if (i % 2 == 0) {
                 ans -= i;
             } else {
                 ans += i;
 
             }
+            i++;
         }
         System.out.println(ans);
 
