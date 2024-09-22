99b3aa356ade7ff45d308faa0e911138ffbf7460
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-22 14:59:08 +0530

    learned concatenation

---
+++ Strings/concatenation.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.*;
+
+public class concatenation {
+    public static void main(String[] args) {
+        String firstName = "Tony";
+        String lastName = "Stark";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+
+}
--- Strings/oneString.java
+++ Strings/oneString.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 public class oneString {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        // String name = sc.nextLine();
-        // System.out.println("your name is : " + name);
-        String name = "Rizwana";
-        String fullName = "Rizwana Perween";
-        String sentence = "My name is Rizwana Perween. ";
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("your name is : " + name);
+        // String name = "Rizwana";
+        // String fullName = "Rizwana Perween";
+        // String sentence = "My name is Rizwana Perween. ";
 
-        System.out.println("Name is  : " + name);
-        System.out.println("Full Name is : " + fullName);
-        System.out.println("Sentence is : " + sentence);
+        // System.out.println("Name is : " + name);
+        // System.out.println("Full Name is : " + fullName);
+        // System.out.println("Sentence is : " + sentence);
     }
 
 }
