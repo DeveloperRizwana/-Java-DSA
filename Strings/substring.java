2679c3ab616e88df760f410eea745c2c8f95ad91
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-22 15:59:17 +0530

    learned to code substring

---
+++ Strings/substring.java
@@ -0,0 +1,25 @@
+package Strings;
+
+import java.util.*;
+
+public class substring {
+    public static void main(String[] args) {
+        // 1st
+        // substring(beggining index, ending index)
+        // String sentence = "My name is Tony";
+        // String name = sentence.substring(11, sentence.length());
+        // System.out.println(name);
+
+        // 2nd
+        // String sentence = "TonyStark";
+        // String name = sentence.substring(0, 4);
+        // System.out.println(name);
+
+        // 3rd
+        String sentence = "TonyStark";
+        String name = sentence.substring(4);
+        System.out.println(name);
+
+    }
+
+}
