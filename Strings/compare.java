d1f489db69610cebd4b83d6db9131459e54d22cf
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-22 15:39:51 +0530

    learned to compare

---
+++ Strings/compare.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.*;
+
+public class compare {
+    public static void main(String[] args) {
+        String name1 = "Hello";
+        String name2 = "World";
+
+        // s1 > s2 : +ve value
+        // s1 < s2 : -ve value
+        // s1 == s2 : 0
+
+        if (name1.compareTo(name2) == 0) {
+            System.out.println("Strings are equal"){ 
+        } else {
+            System.out.println("Strings are not equal");
+        }
+
+    }
+
+}
