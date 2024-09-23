425f419f3d2b410d33c0c972727bee347e4af149
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-23 07:57:22 +0530

    leraned to code insert

---
+++ Strings/insert.java
@@ -0,0 +1,20 @@
+package Strings;
+
+import java.util.*;
+
+public class insert {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+
+        // add a new letter before the word ,,, Tony -> Stony
+        sb.insert(2, 'n'); // sb.insert(0, 'S');
+        System.out.println(sb);
+
+        // delete the extra n
+        sb.delete(2, 3); // ending index non exclusive ,,,,, 3 -> 2
+        System.out.println(sb);
+
+    }
+
+}
