6b6d70fce26310de54d7384d43b82a66b732a7be
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-25 12:04:28 +0530

    learned

---
+++ Array/practiceclass.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.*;
+
+public class practiceclass {
+    public static void main(String[] args) {
+        int[] a = { 10, 20, 30 }; // int[] a = new int[3]; ,,,, int [] a=new int[] {10,20,30};
+        // a[0] = 10;
+        // a[1] = 20;
+        // a[2] = 30;
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+
+}
---
+++ Strings/wordCounter.java
@@ -0,0 +1,16 @@
+package Strings;
+
+import java.util.*;
+
+public class wordCounter {
+    public static void main(String[] args) {
+        String[] words = str.split(" "); // Split the string into words using space as a delimiter
+        return words.length; // Return the number of words
+    }
+
+    public static void main(String[] args) {
+        String input = "Hello world this is ChatGPT"; // Example input
+        int wordCount = countWords(input); // Call the method
+        System.out.println("Total words: " + wordCount); // Output the result
+    }
+}
