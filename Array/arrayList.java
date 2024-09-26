0291359139877eb02ef9ab218279bd9bf227f140
DevloperRizwana <dev.rizwana.perwee.01@gmail.com>
2024-09-26 16:35:47 +0530

    array list

---
+++ Array/arrayList.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.*;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+        list.add(11);
+        list.add(10);
+        list.add(34);
+        list.add(15);
+
+        System.out.println(list);
+
+    }
+
+}
