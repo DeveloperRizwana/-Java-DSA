19309af127560ac5256331a23b26853c1b727a31
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-11-23 10:52:56 +0530

    learned

--- Binary Search/binarySearch.java
+++ Binary Search/binarySearch.java
@@ -33,12 +33,26 @@ public class binarySearch {
         }
         return false;
     }
+    static boolean recursiveSearch(int[] arr, int st, int end, int k){
+        if(st > end) return false;
+
+        int mid = (st+end)/2;
+        if (k == arr[mid]) {
+            return true;
+        } else if(k < arr[mid]){
+            return recursiveSearch(arr, st, mid-1, k);
+        } else {
+            return recursiveSearch(arr, mid+1, end, k);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Binary Search");
         int[] arr = {2, 4, 5, 6, 7};
         int k = 0;
         while (k != 10) {
             System.out.printf("%d exist in arr: %b \n", k, search(arr, k));
+            System.out.printf("%d exist in arr: %b \n", k, recursiveSearch(arr, 0, arr.length-1, k));
+            System.out.println();
             k++;
         }
        
