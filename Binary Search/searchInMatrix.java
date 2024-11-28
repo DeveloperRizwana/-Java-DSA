145438932336f187a09cee584c0570590006c3b0
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-11-28 08:54:03 +0530

    learned to code search in matrix

--- Binary Search/searchInMatrix.java
+++ Binary Search/searchInMatrix.java
@@ -1,4 +1,5 @@
 public class searchInMatrix {
+    // treat the 2d matrix as a 1d sorted array
     static boolean search(int[][] a, int k){
         int n = a.length, m = a[0].length;
         int st = 0, end = n * m - 1;
@@ -14,13 +15,33 @@ public class searchInMatrix {
         }
         return false;
     }
+    static boolean search1(int[][] a, int k){
+        int n = a.length, m = a[0].length;
+        int i = 0, j = m-1;
+        while(i < n && j >= 0){
+            if(a[i][j] == k) return true;
+            if(a[i][j] > k){
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         System.err.println("Result: ");
+        // int[][] a = {
+        //     {1, 3, 5, 7},
+        //     {10, 11, 16, 20},
+        //     {23, 30, 34, 60},
+        // };
+        // System.err.println(search(a, 2));
         int[][] a = {
-            {1, 3, 5, 7},
-            {10, 11, 16, 20},
-            {23, 30, 34, 60},
+            {2, 4, 6},
+            {5, 6, 7},
+            {7, 11, 13},
+            {12, 14, 17},
         };
-        System.err.println(search(a, 2));
+        System.out.println(search1(a, 11));
     }
 }
