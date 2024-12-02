fbfd0317a080284937e60a0b625ed6d75eb1fdfb
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-02 18:13:08 +0530

    learned to code toggle in strings

---
+++ Strings/toggleString.java
@@ -0,0 +1,25 @@
+public class toggleString {
+    public static void main(String[] args) {
+        String s = "PhYsIcS";
+         // toggle -> capital letter to small and small letter to capital
+         for(int i = 0; i < s.length(); i++){
+            boolean flag = true; // true -> capital letter
+            char ch = s.charAt(i); // A
+            if(ch == ' ') continue;
+            int asci = (int)ch; // 65
+            if(asci >= 97) flag = false; // means it's a small letter
+            if(flag == true){ // it's a capital letter
+                asci += 32; // to change in small letter
+                char dh = (char)asci;
+                s = s.substring(0, i) + dh + s.substring(i+1);
+
+            }else { // small
+                asci -= 32;
+                char dh = (char)asci;
+                s = s.substring(0, i) + dh + s.substring(i+1);
+            }    
+        }
+        System.err.println(s);
+    }
+    
+}
