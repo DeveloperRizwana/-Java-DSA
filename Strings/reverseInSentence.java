50527c00bcbb66fc05ff929a2abacb35bea5fe7b
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-02 20:34:33 +0530

    practiced problem of strings

---
+++ Strings/reverseInSentence.java
@@ -0,0 +1,23 @@
+public class reverseInSentence {
+    public static void main(String[] args) {
+        String str = "I am a student";
+        String ans = "";
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch != ' '){
+                sb.append(ch);
+
+            } else {
+                sb.reverse();
+                ans += sb;
+                ans += " ";
+                sb = new StringBuilder("");
+            }
+        }
+        sb.reverse();
+        ans += sb;
+        System.out.println(ans);
+    }
+    
+}
