1ff051af25f58a57060cf519b8bdf7f70c24ac7b
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-03 20:22:20 +0530

    practiced problem

--- Strings/checkPermutation.java
+++ Strings/checkPermutation.java
@@ -1,6 +1,23 @@
 public class checkPermutation {
     public static boolean isPermutation(String str1, String str2) {
-        
+        if (str1 == null || str2 == null) return false;
+        if(str1.length() != str2.length()) return false;
+        int[] arr = new int[125];
+        for(int i = 0; i < str1.length(); i++){
+                arr[str1.charAt(i)]++;
+                arr[str2.charAt(i)]--;
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0) return false;
+        }
+       
+        return true;
     }    
+    public static void main(String[] args) {
+        System.out.println("result: ");
+        String str1 = "abcde";
+        String str2 = "baedc";
+        System.out.println(isPermutation(str1, str2));
+    }
     
 }
