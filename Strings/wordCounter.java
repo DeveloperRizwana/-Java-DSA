1dc93af8ff4a9a508b22a5706e896b66c514a104
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-03 11:47:36 +0530

    practiced problem on coding ninjas

--- Strings/wordCounter.java
+++ Strings/wordCounter.java
@@ -1,16 +1,20 @@
-package Strings;
-
-import java.util.*;
-
 public class wordCounter {
-    public static void main(String[] args) {
-        String[] words = str.split(" "); // Split the string into words using space as a delimiter
-        return words.length; // Return the number of words
+  
+    static int countWords(String str){
+        String[] words = str.split(" ");
+        return words.length;
+        // if(str.length() == 0) return 0;
+        // int count = 1;
+        // for(int i = 0; i < str.length(); i++){
+        //     if(str.charAt(i) == ' '){
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
-
     public static void main(String[] args) {
-        String input = "Hello world this is ChatGPT"; // Example input
-        int wordCount = countWords(input); // Call the method
+        String str = "Hello world this is ChatGPT"; // Example input
+        int wordCount = countWords(str); // Call the method
         System.out.println("Total words: " + wordCount); // Output the result
     }
 }
