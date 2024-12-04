47e00fbbc12ca397c30b348edb8875ab591149d3
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-04 18:06:24 +0530

    learned

--- OOPS/fraction.java
+++ OOPS/fraction.java
@@ -1,4 +1,10 @@
 public class fraction {
+    public static Fraction add(Fraction f1, Fraction f2){
+        int num = f1.numerator*f2.denominator+f2.numerator*f2.denominator;
+        int den = f1.denominator*f2.denominator;
+        Fraction f3 = new Fraction(num, den);
+        return f3;    
+    }
     public static int gcd(int num, int den){
         int min = Math.min(num, den);
         for(int i = min; i >= 1; i--){
@@ -24,8 +30,12 @@ public class fraction {
     public static void main(String[] args) {
         Fraction f1 = new Fraction(3, 18);
         System.out.println("Result: " + f1.numerator+"/"+f1.denominator);
-        // f1.simplify();
-        System.out.println("Result: " + f1.numerator+"/"+f1.denominator);
+        // // f1.simplify();
+        // System.out.println("Result: " + f1.numerator+"/"+f1.denominator);
+        Fraction f2 = new Fraction(7, 3);
+        System.out.println(f2.numerator+"/"+f2.denominator);
+        Fraction f3 = add(f1, f2);
+        System.out.println(f3.numerator+"/"+f3.denominator);
         
     }
     
