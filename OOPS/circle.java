73e61cf5ca5762eb4d9ac139a55efc02d7f3badf
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-11 10:46:17 +0530

    solved problem

--- OOPS/circle.java
+++ OOPS/circle.java
@@ -1,4 +1,23 @@
 public class circle {
-    
-    
+    int radius;
+    double pi = 3.14159;
+
+    public double area(){
+        return pi*radius*radius;
+    }
+
+    public double perimeter(){
+        return 2*pi*radius;
+    }
+
+    public static void main(String[] args) {
+        circle cl = new circle();
+        cl.radius = 7;
+
+        // area of a circle
+        System.out.println("Area of a circle : " + cl.area());
+
+        // perimeter of a circle
+        System.out.println(" perimeter of a circle : " + cl.perimeter());
+    }
 }
