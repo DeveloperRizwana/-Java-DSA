822798a23ad735bd890bc427a8471f0f98c75f44
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-11 10:07:19 +0530

    practiced problem

---
+++ OOPS/cellPhone.java
@@ -0,0 +1,21 @@
+public class cellPhone {
+
+   public void ring(){
+    System.out.println("Ringign.....");
+   }
+
+   public void vibrate(){
+    System.out.println("Vibrating .....");
+   }
+
+   public void callFriend(){
+    System.out.println("Call Nusrat....");
+   }
+
+   public static void main(String[] args) {
+    cellPhone phone = new cellPhone();
+    phone.ring();
+    phone.vibrate();
+    phone.callFriend();
+   }
+}
