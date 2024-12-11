07c459786cb0e4b0436408b4ce51a555cb5d135c
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-11 09:52:43 +0530

    practiced problem

--- OOPS/employeeClass.java
+++ OOPS/employeeClass.java
@@ -4,23 +4,19 @@ public class employeeClass {
 
     public int getSalary(){
         return salary;
-
     }
     public String getName(){
         return name;
     }
     public void setName(String r){
          name = r;
-
     }
-
     public static void main(String[] args) {
         employeeClass rizu = new employeeClass();
         rizu.setName("Rizwana");
         rizu.salary = 10000;
+        
         System.out.println("Salary of rizu is : " + rizu.getSalary());
         System.out.println(rizu.getName());
-
     }
-    
 }
