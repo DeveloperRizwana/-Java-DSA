97363d1b8e2ca0c4cc355755363e52593468fbbf
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-11 10:13:44 +0530

    practiced problem of OOP

--- OOPS/square.java
+++ OOPS/square.java
@@ -1,3 +1,22 @@
 public class square {
+    int side ;
+
+    public int area(){
+        return side * side;
+    }
+
+    public int perimeter(){
+        return 4*side;
+    }
+
+    public static void main(String[] args) {
+        square sq = new square();
+        sq.side = 5;
+        // area of square
+        System.out.println("Area of square : " + sq.area());
+
+        // permeter of square
+        System.out.println("Permeter of square : " + sq.perimeter());
+    }
     
 }
