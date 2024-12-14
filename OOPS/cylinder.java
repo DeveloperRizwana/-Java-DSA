05f3146c853aecb29b344312f124501a0f98da4d
Rizwana Perween <dev.rizwana.perween01@gmail.com>
2024-12-14 06:57:05 +0000

    practiced

--- OOPS/cylinder.java
+++ OOPS/cylinder.java
@@ -4,6 +4,11 @@ public class cylinder {
     double pi = 3.14159;
 
 
+    public cylinder(int radius, int height){
+        this.radius = radius;
+        this.height = height;
+    }
+
     public double surfaceAreaOfCylinder(){
         return 2*(pi*radius*radius) + 2*(pi*height*radius);
     }
@@ -27,11 +32,11 @@ public class cylinder {
     }
 
     public static void main(String[] args) {
-        cylinder cl = new cylinder();
-        cl.setRadius(12);
+        cylinder cl = new cylinder(12, 5);
+        //cl.setRadius(12);
         System.out.println("Radius of a cylinder: " + cl.getRadius());
 
-        cl.setHeight(5);
+        //cl.setHeight(5);
         System.out.println("Height of a cylinder: " + cl.getHeight());
 
         System.out.println("The surface area of a cylinder: " + cl.surfaceAreaOfCylinder());
