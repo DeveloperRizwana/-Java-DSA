411bf9ec41b7af52c8d8ae13fe93fc4fb14c5242
Rizwana Perween <dev.rizwana.perween01@gmail.com>
2024-12-14 11:50:20 +0000

    practiced

--- OOPS/Animal.java
+++ OOPS/Animal.java
@@ -10,10 +10,33 @@ public class Animal {
     public void setLeg(int leg){
         this.leg = leg;
     }
-}
-class dog extends Animal {
+    public String getColor() {
+        return color;
+    }
 
-    public dog(int leg, String color, boolean isPet, String sound){
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean getIsPet() {
+        return isPet;
+    }
+
+    public void setIsPet(boolean isPet) {
+        this.isPet = isPet;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
+class Dog extends Animal {
+
+    public Dog(int leg, String color, boolean isPet, String sound){
         this.leg = leg;
         this.color = color;
         this.isPet = isPet;
@@ -27,13 +50,13 @@ class dog extends Animal {
             pet = "not a pet animal";
 
         }
-        System.out.println("the dog is : " + pet);
-        System.out.println("the dog has : " + this.leg + " legs");
-        System.out.println("the color of the dog is : " + this.color);
+        System.out.println("The dog is : " + pet);
+        System.out.println("The dog has : " + this.leg + " legs");
+        System.out.println("The color of the dog is : " + this.color);
         System.out.println("Sound of dog : " + this.sound);
     }    
     public static void main(String[] args) {
-        dog myDog = new dog(4, "brown", true, "bark bark!");
+        Dog myDog = new Dog(4, "brown", true, "bark bark!");
         myDog.print();
     }
 }
