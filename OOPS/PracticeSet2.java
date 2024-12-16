9d2684337b5b914c052d644c534ec1013d7ac35a
Rizwana Perween <dev.rizwana.perween01@gmail.com>
2024-12-16 11:46:12 +0000

    demonstrated polymorphism

--- OOPS/PracticeSet2.java
+++ OOPS/PracticeSet2.java
@@ -5,20 +5,31 @@ abstract class Telephone{
 }
 class SmartTelephone extends Telephone{
 
+    public void recordingVideo(){
+        System.out.println("recording video");
+    }
+
     public void ring(){
+        System.out.println("ringing....");
 
     }
     public void lift(){
-
+        System.out.println("lifting...");
     }
     public void disconnect(){
+        System.out.println("diconnecting the telephone");
 
     }
 }
 
 public class PracticeSet2 {
     public static void main(String[] args) {
-        
+        Telephone t = new SmartTelephone();
+        t.disconnect();
+        t.lift();
+        t.disconnect();
+
+        SmartTelephone smart = new SmartTelephone();
+        smart.recordingVideo();
     }
-    
 }
