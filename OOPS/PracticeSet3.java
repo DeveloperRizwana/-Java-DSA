78ed11412c6a472f2c1d0b17f21af150baf6f9fd
Rizwana Perween <dev.rizwana.perween01@gmail.com>
2024-12-16 14:12:51 +0000

    practiced problems of interfaces

---
+++ OOPS/PracticeSet3.java
@@ -0,0 +1,42 @@
+interface TvRemote{
+    void changeChanel();
+    void increaseVolume();
+}
+class SmartTvRemote implements TvRemote{
+    @Override
+    public void changeChanel(){
+        System.out.println("Change this chanel with the smart tv remote");
+    }
+
+    @Override
+    public void increaseVolume(){
+        System.out.println("Increase the volume with the smart tv remote");
+    }
+}
+class Tv implements TvRemote{
+
+    @Override
+    public void changeChanel(){
+        System.out.println("Change this chanel from the tv");
+    }
+
+    @Override
+    public void increaseVolume(){
+        System.out.println("Increase the volume of tv");
+    }
+
+}
+
+public class PracticeSet3 {
+    public static void main(String[] args) {
+        Tv t = new Tv();
+        t.changeChanel();
+        t.increaseVolume();
+
+        SmartTvRemote sr = new SmartTvRemote();
+        sr.changeChanel();
+        sr.increaseVolume();
+        
+    }
+    
+}
